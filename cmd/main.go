68d9a7441ddffc99266dfd243c2affd888ee9484
Ермаков Кирилл <gem28mm69@gmail.com>
2026-05-02 18:21:57 +0300

    init commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
+	server "sprint-6/internal/server"
+)
 
+func main() {
+	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
+	serv := server.CreateServer(logger)
+
+	logger.Fatal(serv.HTTPServer().ListenAndServe())
 }
